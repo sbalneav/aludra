802de714fb4d0813438f9995514af140cecea13b
Scott Balneaves <sbalneav@alburg.net>
2012-08-24 14:54:25 -0500

    Some testing infrastructure: very small dummy filesystem

---
+++ sql/test_filesystem.sql
@@ -0,0 +1,36 @@
+--
+-- staticallty defined test filesystem
+--
+
+--
+-- Root directory
+--
+
+-- INSERT INTO INODE
+-- (path, name, st_dev, st_mode, st_nlink, st_uid, st_gid, st_rdev, st_size, st_blksize, st_blocks, st_atime, st_mtime, st_ctime)
+--  VALUES ('/', '/', 0, 16877, 3, 0, 0, 0, 4096, 0, 0, unixtime(), unixtime(), unixtime());
+
+--
+-- "test" directory owned by root.
+--
+
+-- INSERT INTO INODE
+--  (path, name, st_dev, st_mode, st_nlink, st_uid, st_gid, st_rdev, st_size, st_blksize, st_blocks, st_atime, st_mtime, st_ctime)
+--  VALUES ('/', 'test', 0, 16877, 3, 0, 0, 0, 4096, 0, 0, unixtime(), unixtime(), unixtime());
+
+--
+-- "flarb" directory owned by uid/groupid 1111.
+--
+
+-- INSERT INTO INODE
+--  (path, name, st_dev, st_mode, st_nlink, st_uid, st_gid, st_rdev, st_size, st_blksize, st_blocks, st_atime, st_mtime, st_ctime)
+--  VALUES ('/', 'flarb', 0, 16877, 3, 1111, 1111, 0, 4096, 0, 0, unixtime(), unixtime(), unixtime());
+
+--
+-- "foo.txt" file owned by uid/groupid 1111.
+--
+
+INSERT INTO INODE
+ (path, name, st_dev, st_mode, st_nlink, st_uid, st_gid, st_rdev, st_size, st_blksize, st_blocks, st_atime, st_mtime, st_ctime)
+ VALUES ('/', 'foo.txt', 0, 33188, 3, 1111, 1111, 0, 4096, 0, 0, unixtime(), unixtime(), unixtime(), 1);
+
