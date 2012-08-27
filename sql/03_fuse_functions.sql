dc26f6374de5e75de3ce8ff5b8231aa3fd89eb19
Scott Balneaves <sbalneav@alburg.net>
2012-08-27 15:01:04 -0500

    Modified getattr to return whole row

--- sql/03_fuse_functions.sql
+++ sql/03_fuse_functions.sql
@@ -3,38 +3,19 @@
 -- operations of Aludra
 --
 
-DROP TYPE statbuf CASCADE;
-CREATE TYPE statbuf AS
-  (
-    st_dev     INTEGER,
-    st_ino     INTEGER,
-    st_mode    INTEGER,
-    st_nlink   INTEGER,
-    st_uid     INTEGER,
-    st_gid     INTEGER,
-    st_rdev    INTEGER,
-    st_size    INTEGER,
-    st_blksize INTEGER,
-    st_blocks  INTEGER,
-    st_atime   INTEGER,
-    st_mtime   INTEGER,
-    st_ctime   INTEGER
-  );
-
 --
 -- getattr
 --
 
-CREATE OR REPLACE FUNCTION getattr (abspath TEXT) RETURNS SETOF RECORD AS $$
+CREATE OR REPLACE FUNCTION getattr (abspath TEXT) RETURNS SETOF inode AS $$
 DECLARE
     mypath  TEXT;
     myname  TEXT;
---    ret     statbuf%ROWTYPE;
 BEGIN
     mypath  := dirname(abspath);
     myname  := basename(abspath);
 
-    SELECT (st_dev, st_ino, st_mode, st_nlink, st_uid, st_gid, st_rdev, st_size, st_blksize, st_blocks, st_atime, st_mtime, st_ctime)
+    RETURN QUERY SELECT *
       FROM inode
       WHERE path = mypath AND name = myname;
 END;
